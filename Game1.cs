c3dc063944aaab94502cbd4c17c796c80d08ff5f
Kevin Östling <kevost0611@staden.gotheborg.net>
2023-12-08 10:07:42 +0100

    Slut

--- Game1.cs
+++ Game1.cs
@@ -3,6 +3,7 @@ using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
 using System;
 using System.Security.Cryptography;
+using static System.Formats.Asn1.AsnWriter;
 
 namespace Spelprojekt_Kevin_Ö
 {
@@ -12,15 +13,18 @@ namespace Spelprojekt_Kevin_Ö
         private SpriteBatch spriteBatch;
 
         Texture2D spaceBild;
-        Texture2D bucketBild;
+        Texture2D basketBild;
         Texture2D starBild;
 
+        SpriteFont Times;
+
+        int score = 0;
        
         Vector2 spacePosition = new Vector2(0, 0);
         KeyboardState tangentbord = Keyboard.GetState();
         Random RandomNummer = new Random();
-        Rectangle starRectangle = new Rectangle (300, 100, 0, 0);
-        Rectangle bucketRectangle = new Rectangle(550, 550, 450, 250);
+        Rectangle starRectangle = new Rectangle(300, 100, 80, 80);
+        Rectangle basketRectangle = new Rectangle(550, 550, 250, 210);
         
         public Game1()
         {
@@ -44,8 +48,10 @@ namespace Spelprojekt_Kevin_Ö
         {
             spriteBatch = new SpriteBatch(GraphicsDevice);
             spaceBild = Content.Load<Texture2D>("space");
-            bucketBild = Content.Load<Texture2D>("bucket");
+            basketBild = Content.Load<Texture2D>("basket");
             starBild = Content.Load<Texture2D>("star");
+
+            Times = Content.Load<SpriteFont>("Times");
 
             // TODO: use this.Content to load your game content here
         }
@@ -60,14 +66,22 @@ namespace Spelprojekt_Kevin_Ö
             tangentbord = Keyboard.GetState();
             if (Keyboard.GetState().IsKeyDown(Keys.Left))
             {
-                bucketRectangle.X -= 10;
+                basketRectangle.X -= 10;
             }
             if (Keyboard.GetState().IsKeyDown(Keys.Right))
             {
-                bucketRectangle.X += 10;
+                basketRectangle.X += 10;
             }
 
-                if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+            if (basketRectangle.Intersects(starRectangle))
+            {
+                starRectangle = new Rectangle(Random, 20, 80, 80);
+                score++;
+            }
+
+            base.Update(gameTime);
+
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
                 Exit();
 
             // TODO: Add your update logic here
@@ -80,10 +94,13 @@ namespace Spelprojekt_Kevin_Ö
             GraphicsDevice.Clear(Color.CornflowerBlue);
 
             spriteBatch.Begin();
-            spriteBatch.Draw(starBild, starRectangle, Color.White);
             spriteBatch.Draw(spaceBild, spacePosition, Color.White);
-            spriteBatch.Draw(bucketBild, bucketRectangle, Color.White);
+            spriteBatch.Draw(basketBild, basketRectangle, Color.White);
+            spriteBatch.Draw(starBild, starRectangle, Color.White);
+            spriteBatch.DrawString(Times, score.ToString(), Vector2.Zero, Color.White);
             spriteBatch.End();
+           
+          
             // TODO: Add your drawing code here
 
             base.Draw(gameTime);
